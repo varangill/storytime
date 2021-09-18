d1cb013b077c9ebc226d3197cf8ecc3873bfb8c0
Varandeep Gill <gillzx01@gmail.com>
2021-09-18 18:10:16 -0400

    Backend skeleton

---
+++ storytime/src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmwDRoubN9yq_OyqPJkPGhphVH3J4zoSQ",
+    authDomain: "storyline-9a9b2.firebaseapp.com",
+    databaseURL: "https://storyline-9a9b2-default-rtdb.firebaseio.com/",
+    projectId: "storyline-9a9b2",
+    storageBucket: "storyline-9a9b2.appspot.com",
+    messagingSenderId: "759495346730",
+    appId: "1:759495346730:web:604ced3a71dc0e56459153",
+    measurementId: "G-07EE54B2YT"
+};
+
+firebase.initializeApp(config);
