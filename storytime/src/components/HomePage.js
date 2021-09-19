094d76a73dcd1d0eed11aee6736175d66dce3eba
3ricLu <EricLu.Canada@gmail.com>
2021-09-18 20:10:34 -0400

    css

--- storytime/src/components/HomePage.js
+++ storytime/src/components/HomePage.js
@@ -4,6 +4,38 @@ import { Link, useHistory } from 'react-router-dom';
 function HomePage() {
   const history = useHistory();
 
+  
+  const title = {
+    fontSize: 50,
+    color: "#4a54f1",
+    textAlign: "center",
+    paddingTop: "100px",
+  }
+
+
+  const subtitle = {
+    fontSize: 20,
+    color: "#4a54f1",
+    textAlign: "center",
+    paddingTop: "0px",
+  }
+
+  const buttonStyle1 = {
+    fontSize: 20,
+    color: "#4a54f1",
+    textAlign: "center",
+    paddingTop: "0px",
+    paddingRight: "50%",
+    paddingLeft: "50%"
+
+  }
+
+  const filler = {
+    fontSize: 20,
+    color: "#4a54f1",
+    textAlign: "center",
+    paddingTop: "200px",
+  }
 
   const createPublicRoom = () => {
     var code = "";
@@ -17,9 +49,13 @@ function HomePage() {
   }
 
   return (
-    <div>
-        <h2>a cozy home</h2>
-        <button onClick={createPublicRoom}>Create Public Room</button>
+    <div style={{color: "#CCEEFF", backgroundColor: "#CCEEFF"}}>
+        <h2 style={title}>Story Time</h2>
+        <h3 style={subtitle}>An interactive story teller</h3>
+        <h4 style={filler}></h4>
+          
+        <button onClick={createPublicRoom} style={buttonStyle1}>Create Public Room</button>
+
     </div>
   );
 }
